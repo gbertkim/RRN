d61e730efc88bfdab27489b985c13de115473d4e
Gilbert Kim <gbertkim@gmail.com>
2019-03-15 09:19:13 -0700

    1st working demo of app

--- script.js
+++ script.js
@@ -8,56 +8,68 @@ let locationInfo = [];
 let markers = [];
 let imageId;
 let map;
+let flightPath;
+
+// Icon URLS
+let flag = {
+    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+} 
+let green = {
+    url: 'https://www.google.com/mapfiles/arrow.png',
+}
+let rrn = {
+    url: 'https://i.ibb.co/wpX8d1r/Webp-net-resizeimage-1.png',
+}
 
 let STORE = [{
     date: 'May 3, 2019',
     venue: "High Watt",
+    city: 'Nashville, TN',
     location: {lat: 36.1530, lng: -86.7806},
-    link: 'https://www.eventbrite.com/e/run-river-north-tickets-55663808951?aff=aff0bandsintown&appId=wf_ubzr.ehaevireabegu.pbz&comeFrom=242&artist_event_id=100802634'
+    link: 'https://www.eventbrite.com/e/run-river-north-tickets-55663808951?aff=aff0bandsintown&appId=wf_ubzr.ehaevireabegu.pbz&comeFrom=242&artist_event_id=100802634',
+    image: 'https://media-cdn.tripadvisor.com/media/photo-s/0f/c8/b4/e9/the-high-watt-has-a-225.jpg'
     },{
     date: 'May 4, 2019',
     venue: "Neighborhood Theatre",
+    city: "Charlotte, NC",
     location: {lat: 35.2474, lng: -80.804},
-    link: 'https://www.ticketfly.com/purchase/event/1825144/tfly?utm_medium=%5BLjava.lang.String%3B%4057891044&skinName=tfly'
+    link: 'https://www.ticketfly.com/purchase/event/1825144/tfly?utm_medium=%5BLjava.lang.String%3B%4057891044&skinName=tfly',
+    image: 'http://photos.cinematreasures.org/production/photos/167026/1462716639/large.jpg?1462716639'
     },{
     date: 'May 7, 2019',
     venue: "Union Stage",
+    city: 'Washington, DC',
     location: {lat: 38.8787,lng: -77.0241},
-    link: 'https://www.ticketfly.com/purchase/event/1823442/tfly?utm_medium=%5BLjava.lang.String%3B%406d48ffee&skinName=tfly'
+    link: 'https://www.ticketfly.com/purchase/event/1823442/tfly?utm_medium=%5BLjava.lang.String%3B%406d48ffee&skinName=tfly',
+    image: 'https://cdn0.weddingwire.com/emp/fotos/1/6/6/7/8/9/1507555083412-union-stage-3.jpg'
 }];
-
-
 
 function initMap() {
     // add map with center and zoom
-        map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 39.8283, lng: -98.5795}, 
-        zoom: 4
+        zoom: 3
     });
+    $('#mapKeys').html(`
+        <ul id="mapIcons">
+            <li>Past Dates = <img src=${flag.url} alt="Flag Icon" width="15" height="20"></li>
+            <li>Current Location = <img src=${rrn.url} alt="Run River North Icon" width="15" height="20"></li>
+            <li>Future Dates = <img src=${green.url} alt="Green Arrow Icon" width="15" height="20"</li>
+        </ul>
+    `)
 }
 
 
 function drop() {
-    // On the click of the drop button in html run this function
-   
-    // Icon URLS
-    // One object
-    let flag = {
-        url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-    } 
-    let green = {
-        url: 'https://www.google.com/mapfiles/arrow.png',
-    }
-    let rrn = {
-        url: 'https://i.ibb.co/wpX8d1r/Webp-net-resizeimage-1.png',
-    }
-
-    // Clear any previous markers
+// Clear any previous markers
     clearMarkers();
+    if (flightPath != undefined){
+        clearFlight();
+    }
     pastLocations(flag);
     setTimeout(function(){
         currentLocation(rrn);
-    }, 250);
+    }, 500);
     setTimeout(function(){
         futureLocations(green);
         addListeners();
@@ -69,34 +81,38 @@ function drop() {
         };
         myoverlay.setMap(map);
 
-        //function for flight path    
-        var flightPath = new google.maps.Polyline({
-            path: locationInfo,
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        });
-        flightPath.setMap(map);
-    }, 500);
+        makePath();
+
+    }, 1000);
     console.log(locationInfo);
+}
 
-
-
-    //Creates a way to target each marker
-    function addListeners(){
-        for (let i = 0; i<markers.length; i++){
-            markers[i].addListener('click', function(){
-                let thisTitle = Number(this.title);
-                $('#markerLayer img').eq(thisTitle).parent().addClass("bounce").siblings().removeClass("bounce");
-                $(`#markerLayer img[src='https://i.ibb.co/wpX8d1r/Webp-net-resizeimage-1.png']`).css('animation','none');
-                imageId = thisTitle;
-                console.log(thisTitle);
-                uploadPicture(imageId);
-            });
-        }
+//function for flight path    
+function makePath(){
+    flightPath = new google.maps.Polyline({
+        path: locationInfo,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    flightPath.setMap(map);
+}
+//Creates a way to target each marker
+function addListeners(){
+    for (let i = 0; i<markers.length; i++){
+        markers[i].addListener('click', function(){
+            let thisTitle = Number(this.title);
+            $('#markerLayer img').eq(thisTitle).parent().addClass("bounce").siblings().removeClass("bounce");
+            $(`#markerLayer img[src='https://i.ibb.co/wpX8d1r/Webp-net-resizeimage-1.png']`).css('animation','none');
+            imageId = thisTitle;
+            console.log(thisTitle);
+            uploadPicture(imageId);
+            $('#infoContainer').slideDown();
+        });
     }
 }
+
 // Create markers for past dates
 function pastLocations(icon){
     for (let i = 0; i < imageFinal.length-1; i++) {
@@ -106,7 +122,7 @@ function pastLocations(icon){
 }
 
 // Create markers for where they last were
- function currentLocation(icon){
+function currentLocation(icon){
     addMarker(imageFinal[imageFinal.length-1].location, icon)
     locationInfo.push(imageFinal[imageFinal.length-1].location);
 }
@@ -121,7 +137,51 @@ function futureLocations(icon){
 
 
 function uploadPicture(num){
-    $('#imageRight').attr('src',`${imageFinal[num].url}`)
+    if (num+1>imageFinal.length){
+        let storeNum = num-imageFinal.length
+        $('#imageRight').attr('src',`${STORE[storeNum].image}`);
+        $('#imageDescription').html(`
+            <div class="listBox">
+                <ul class="list">
+                    <li>Date: ${STORE[storeNum].date}</li>
+                    <li>Venue: ${STORE[storeNum].venue}</li>
+                    <li>Location: ${STORE[storeNum].city}</li>
+                    <li>Ticket: <a class="tixLink" href="${STORE[storeNum].link}">Link</a></li>
+                </ul>
+            </div>
+            <div class="arrowBox">
+                <a href="#full">
+                    <div id="arrow"></div>
+                </a>
+            </div>
+        `)
+        arrowMaker();
+    } else {
+        $('#imageRight').attr('src',`${imageFinal[num].url}`);
+        $('#imageDescription').html(`
+            <div class="listBox">
+                <ul class="list">
+                    <li>Date: ${imageFinal[num].time}</li>
+                    <li>Location: ${imageFinal[num].city}</li>
+                    <li>Description: ${imageFinal[num].text}</li>
+                </ul>
+            </div>
+            <div class="arrowBox">
+                <a href="#full">
+                    <div id="arrow"></div>
+                </a>
+            </div>
+        `)
+        arrowMaker();
+    }
+}
+
+function arrowMaker(){
+    $('.arrowBox').on('click', function(event){
+        event.preventDefault();
+        console.log('Arrow Clicked');
+        $('#infoContainer').slideUp();
+    });
 }
 
 // Template for making markers
@@ -146,7 +206,9 @@ function clearMarkers(){
     locationInfo = [];
 }
 
-
+function clearFlight(){
+    flightPath.setMap(null);
+}
 
 // GET request to instagram 
 function instagramAPI(){
@@ -168,7 +230,8 @@ function instagramAPI(){
 // Filter the results from GET request
 function displayResults(responseJson){
     // Filter reponse from instagram to responses with the correct hashtag
-    let filtered = responseJson.data.filter(function(image){
+    console.log(responseJson);
+        let filtered = responseJson.data.filter(function(image){
         return image.tags == 'faketour2019';
     });
     console.log(filtered);
@@ -176,18 +239,23 @@ function displayResults(responseJson){
     filtered.sort(function(a,b){
         return a.caption.created_time - b.caption.created_time;
     });
+
     // Create an array with only needed information
     for (let i=0; i<filtered.length; i++){
         imageFinal[i] = {
             url: filtered[i].images.standard_resolution.url,
-            time: filtered[i].caption.created_time,
+            time: new Date(filtered[i].caption.created_time * 1000).toDateString(),
             text: filtered[i].caption.text,
+            city: filtered[i].location.name,
             location: {
                 lat: filtered[i].location.latitude,
                 lng: filtered[i].location.longitude
             }
         };
     }
+    var date = new Date(filtered[1].caption.created_time * 1000);
+    console.log(filtered[1].caption.created_time);
+    console.log(date);
     console.log(imageFinal);
     // Call map making function
     initMap();
@@ -195,7 +263,10 @@ function displayResults(responseJson){
 
 ///////////Form Load
 function loadForm(){
-    $('#quizStart').on('click', '#drop', function(){
+    appendKeys();
+    $('#infoContainer').hide();
+    instagramAPI();
+    $('#dropMarkers').on('click', '#drop', function(){
         console.log("Searching Instagram API");
         drop();    
     });
@@ -209,9 +280,6 @@ function appendKeys(){
     );
 }
 
-instagramAPI();
-
-appendKeys();
 loadForm();
 
 // animation to drop pin in order
